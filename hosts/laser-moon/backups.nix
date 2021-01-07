e23daad6ff417a8ae536f4640082bce3c1e83daa
Roxie Gibson <me@roxxers.xyz>
2021-01-07 19:09:35 +0000

    chore: changed timing of home backups to every 4h

--- hosts/laser-moon/backups.nix
+++ hosts/laser-moon/backups.nix
@@ -4,7 +4,7 @@
   services.borgbackup.jobs = {
     documents = {
       user = "roxie";
-      startAt = "20:00:00"; # https://www.man7.org/linux/man-pages/man7/systemd.time.7.html
+      startAt = "0 */4 * * *"; # every 4 hours cronjob style https://www.man7.org/linux/man-pages/man7/systemd.time.7.html
       paths = [ "/home/roxie" ];
       repo = "ssh://u218033@u218033.your-storagebox.de:23/./backups/documents";
       exclude = [ "*.nobackup" "/home/roxie/.cache" "/home/roxie/Downloads" "*/node_modules" "*/*venv" ];
@@ -16,6 +16,7 @@
       prune = {
         keep = {
           daily = 7;
+          within = "1d";
         };
       };
     };
