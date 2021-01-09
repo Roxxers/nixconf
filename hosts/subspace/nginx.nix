0bd803d58054f7d56272613c90ac403f98341d05
Roxie Gibson <me@roxxers.xyz>
2021-01-09 04:11:31 +0000

    feat: added test reverse proxy for nextcloud
    testing the ability for gateway to proxy into subspace servers

--- hosts/subspace/nginx.nix
+++ hosts/subspace/nginx.nix
@@ -1,21 +1,42 @@
 {...}:
 {
   networking.firewall = {
-    allowedTCPPorts = [ 443 ];
+    allowedTCPPorts = [ 80 443 ];
   };
   services.nginx = {
     enable = true;
-    virtualHosts."onion.queerdorks.club" = {
-      enableACME = true;
-      forceSSL = true;
-      root = "/var/www/onion";
-      basicAuth = { roxie = builtins.readFile ../../secrets/subspace/onionlistingpw;};
+    recommendedProxySettings = true;
+    #recommendedTlsSettings = true;
+    sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+    virtualHosts = {
+      "onion.queerdorks.club" = {
+        enableACME = true;
+        forceSSL = true;
+        root = "/var/www/onion";
+        basicAuth = { 
+          roxie = builtins.readFile ../../secrets/subspace/onionlistingpw;
+        };
+      };
+      "cloudn.queerdorks.club" = {
+        enableACME = true;
+        forceSSL = true;
+        locations = {
+          "/" = {
+            proxyPass = "https://cloud.awoo";
+          };
+        };
+        basicAuth = { 
+          roxie = builtins.readFile ../../secrets/subspace/cloudproxy_pw_roxie;
+          emily = builtins.readFile ../../secrets/subspace/cloudproxy_pw_emily ;
+        };
+      };
     };
   };
   # Optional: You can configure the email address used with Let's Encrypt.
   # This way you get renewal reminders (automated by NixOS) as well as expiration emails.
   security.acme.certs = {
     "onion.queerdorks.club".email = "me@roxxers.xyz";
+    "cloudn.queerdorks.club".email = "me@roxxers.xyz";
   };
   security.acme.acceptTerms = true;
 }
