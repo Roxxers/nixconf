fc2a69f5c1c7982e24df0ac7d59916014ca1895e
Roxie Gibson <me@roxxers.xyz>
2021-01-15 14:12:48 +0000

    feat: added nur to default packages

--- common/default.nix
+++ common/default.nix
@@ -14,4 +14,9 @@
     trustedUsers = [ "root" "roxie" ];
     autoOptimiseStore = true;
   };
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
 }
